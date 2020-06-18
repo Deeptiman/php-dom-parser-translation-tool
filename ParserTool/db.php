8f92eb71784c8405edfb854fe1e1eed75c63934e
Deeptiman Pattnaik <pattnaikdeeptiman@gmail.com>
2020-06-18 10:00:41 +0300

    Update db.php

--- ParserTool/db.php
+++ ParserTool/db.php
@@ -1,7 +1,7 @@
 <?php
 
 $db=mysql_connect("localhost","root","");
-mysql_select_db("odia_unicode",$db);
+mysql_select_db("dom_parser",$db);
 
 mysql_query("SET CHARACTER SET utf8");
 mysql_query("SET NAMES utf8");

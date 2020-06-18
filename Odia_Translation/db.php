3b08cec1b2bb8e25dae4f4f38124506db85e554e
Deeptiman Pattnaik <pattnaikdeeptiman@gmail.com>
2020-06-18 10:01:32 +0300

    Update db.php

--- Odia_Translation/db.php
+++ Odia_Translation/db.php
@@ -1,6 +1,6 @@
 <?php
 $db=mysql_connect("localhost","root","")or die(mysql_error());
-        mysql_select_db("new_google",$db);
+        mysql_select_db("odia_translate",$db);
 		
 		mysql_query("SET CHARACTER SET utf8");
 mysql_query("SET NAMES utf8");
